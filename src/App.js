b9684e96dee0e38952151255f3472177ab41fe50
jluna101 <jfluna1996@gmail.com>
2022-04-11 13:15:21 -0700

    Added prices,news, and transactions routes

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import CoinbaseTransactions from './components/CoinbaseTransactions';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 
 function App() {
   return (
     <div>
       <Header/>
       <Navbar/>
-      <CryptoPrices/>
-      <CryptoNews/>
-      <CoinbaseTransactions/>
+      <Routes>
+        <Route path='/prices'element={<CryptoPrices/>} />
+        <Route path='/news' element= {<CryptoNews/>} /> 
+        <Route path='/transactions' element= {<CoinbaseTransactions/>} /> 
+        
+      </Routes>
       <Footer/>
     </div>
   );
