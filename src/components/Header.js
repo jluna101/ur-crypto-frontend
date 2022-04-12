867ededf044d4ee1bca872dfa63702e32527428d
jluna101 <jfluna1996@gmail.com>
2022-04-12 16:45:51 -0700

    Fixed conditional rendering issue in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header(props) {
-    const data = props.coinData;
+  const data = props.coinData;
     function integer(num){
         return parseInt((num).toFixed(0)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}   
   return (
     <div>
-        {(data)? 
+        {(data.length > 1)? 
         <nav className="navbar py-0">
-            <ul className="nav-item m-2">BTC Price: ${integer(data[0].price)}</ul>
-            <ul className="nav-item m-2">ETH Price: ${integer(data[1].price)}</ul>
-            <ul className="nav-item m-2">Solana Price: ${integer(data[6].price)}</ul>
-            <ul className="nav-item m-2">ETC Price: ${integer(data[27].price)}</ul>
+          <ul className="nav-item m-2">BTC PRICE: ${integer(data[0].price)}</ul>
+          <ul className="nav-item m-2">ETH Price: ${integer(data[1].price)}</ul>
+          <ul className="nav-item m-2">Solana Price: ${integer(data[6].price)}</ul>
+          <ul className="nav-item m-2">ETC Price: ${integer(data[27].price)}</ul>
         </nav>: <div>Loading</div>}
     </div>
   );
