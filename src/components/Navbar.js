8e29a4e083acbf020bfaa2938bf3eae0c389b71b
jluna101 <jfluna1996@gmail.com>
2022-05-03 17:39:24 -0700

    Updated navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,34 +2,38 @@ import React from 'react';
 import { Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Switch from 'react-js-switch';
+import Button from 'react-bootstrap/Button'
 
 
 function Navbar({signedIn, handleSignout, userInfo, theme, toggleTheme}) {
-    function uppercaseFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
+    // Makes first letter of word Uppercase - NOT IN USE
     return (
-        <nav className="navbar navbar-dark bg-dark">
-            <div className="container-fluid ">
-            <Link to='/homepage' className="text-decoration-none navbar-brand">urCrypto</Link>
-            <Link to='/prices' className="text-decoration-none navbar-brand">Prices</Link>
-            <Link to='/news' className="text-decoration-none navbar-brand">News</Link>
-            {userInfo !== null ? (
-							<NavItem className="navbar-brand">
-								Welcome {uppercaseFirstLetter(userInfo.username)}!
-							</NavItem>
-						): <></>}
+        <div>
+            {/* Signup Banner */}
+            {(signedIn === false)&& 
+            <div className='bg-primary  py-2 px-auto d-flex justify-content-center'>
+                <Link to='/signup' className='text-white text-decoration-none'>
+                    Sign up for full access
+                </Link>
+            </div>}
+        <nav className="bg-white text-black border-bottom border-grey py-2 px-5">
+            <div className="d-flex justify-content-between align-items-center">
+            <Link to='/homepage' className="text-decoration-none text-primary fs-3 fw-bold">urCrypto</Link>
+            <Link to='/prices' className="text-decoration-none text-black">Prices</Link>
+            <Link to='/news' className="text-decoration-none text-black">News</Link>
             {(signedIn === true) ? (
-                <><Nav.Link className="text-decoration-none" onClick={handleSignout}>Signout</Nav.Link></>
-                ):( 
                 <>
-                    <Link to='/' className="text-decoration-none navbar-brand">Sign-in</Link>
-                    <Link to='/signup' className="text-decoration-none navbar-brand">Sign-up</Link>
-                </>
-                )}
-            <Switch onChange={toggleTheme}/>
+                    <Nav.Link className="text-decoration-none text-black" onClick={handleSignout}>
+                        Signout
+                    </Nav.Link>
+                </> ):(<>
+                    <Link to='/' className="text-decoration-none text-black">Sign-in</Link>
+                    <Link to='/signup' className="text-decoration-none text-black"><Button variant='primary'>Sign-up</Button></Link>
+                </>)}
+                <Switch Change={toggleTheme} content='word'/>
             </div>
         </nav>
+        </div>
     );
 }
 
